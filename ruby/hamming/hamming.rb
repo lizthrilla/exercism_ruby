ca2230e14b1609f9556f50d9551913110b59f073
Liz Tiller <elizabeth.tiller@gmail.com>
2017-03-02 15:22:23 -0500

    added notes

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,7 +1,8 @@
 class Hamming
   def self.compute(a, b)
-    #basic comparison test to check ruby syntax
+    #can put the resolution before the conditional statement
     raise ArgumentError if a.length != b.length
+    #basic comparison test to check ruby syntax
     if a == b
       0
     end
