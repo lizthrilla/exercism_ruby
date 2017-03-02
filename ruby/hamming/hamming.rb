9e41d7999142be64a9a339fa3f493a7a7ba56e90
Liz Tiller <elizabeth.tiller@gmail.com>
2017-03-01 23:32:46 -0500

    look up string functions

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,7 +1,9 @@
 class Hamming
   def self.compute(a, b)
+    #basic comparison test to check ruby syntax
     if a == b
       0
     end
+    #look up .count and .each_char string functions
   end
 end
